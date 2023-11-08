fa11ecbe22a50ed698d18f1aa9bf8706e3d7071b
Ankit Kumar <ankitbh2021@gmail.com>
2023-11-08 20:27:46 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Expenses from './components/Expenses/Expenses';
+
+import NewExpense from './components/NewExpense/NewExpense';
+
+let DUMMY_EXPENSE = [
+    {
+        id : 'e1',
+        title : 'School fee',
+        amount : 800,
+        date : new Date (2023 , 5 , 12)
+    },
+
+    {
+        id : 'e2',
+        title : 'Books',
+        amount : 250,
+        date : new Date (2023 , 4 , 2)
+    },
+
+    {
+        id : 'e3',
+        title : 'Tuition fee',
+        amount : 350,
+        date : new Date (2023 , 3 , 1)
+    },
+
+    {
+        id : 'e4',
+        title : 'Sports',
+        amount : 550,
+        date : new Date (2023 , 4 , 6)
+    }
+];
+
+function App(){
+    
+    const [expenses , setExpenses] = useState(DUMMY_EXPENSE);
+
+    const addExpenseHandler = (expense) =>{
+        const updatedExpense = [expense , ...expenses];
+        setExpenses(updatedExpense);
+        
+
+    }
+    return(
+        <div>
+            <NewExpense onAddExpense = {addExpenseHandler}/>
+            <Expenses  item = {expenses}/>
+     
+        </div>
+       
+    ); 
 }
 
 export default App;
